8d74e3a6f8008e1434a0799d811dbeeb3e0ab34f
janisha <65414656+janisha13@users.noreply.github.com>
2021-02-05 09:55:23 -0600

    Operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+         
+        }
+    }
+}
